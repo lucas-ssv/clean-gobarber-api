728924a29739832dd796bf7cff8c9dba29c2fcb1
Lucas <lucas_99silva@hotmail.com>
2022-07-20 15:43:48 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,11 +1,25 @@
 import { DbAddAccount } from './db-add-account'
 import { AddAccountRepositoryStub, mockFakeAddAccountRequest } from '../../tests/mock-add-account-repository'
+import { AddAccountRepository } from '../../protocols/add-account-repository'
+
+type SutTypes = {
+  sut: DbAddAccount
+  addAccountRepositoryStub: AddAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addAccountRepositoryStub = new AddAccountRepositoryStub()
+  const sut = new DbAddAccount(addAccountRepositoryStub)
+  return {
+    sut,
+    addAccountRepositoryStub
+  }
+}
 
 describe('DbAddAccount usecase', () => {
   test('Should call AddAccountRepository with correct values', async () => {
-    const addAccountRepositoryStub = new AddAccountRepositoryStub()
+    const { sut, addAccountRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
-    const sut = new DbAddAccount(addAccountRepositoryStub)
     const fakeAddAccountRequest = mockFakeAddAccountRequest()
     await sut.add(fakeAddAccountRequest)
     expect(addSpy).toHaveBeenCalledWith(fakeAddAccountRequest)
