49f2938093bd2d4602e21141c7ba44025240c809
Lucas <lucas_99silva@hotmail.com>
2022-07-25 11:56:50 -0300

    feat: ensure DbAddAccount calls Encrypter with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,12 +1,17 @@
 import { AccountModel } from '../../../domain/models/account'
 import { AddAccount, AddAccountParams } from '../../../domain/usecases/add-account'
-import { AddAccountRepository } from '../../protocols/add-account-repository'
+import { AddAccountRepository } from '../../protocols/db/add-account-repository'
+import { Encrypter } from '../../protocols/encrypt/encrypter'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly addAccountRepository: AddAccountRepository) {}
+  constructor (
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly encrypter: Encrypter
+  ) {}
 
   async add (account: AddAccountParams): Promise<AccountModel> {
-    const newAccount = await this.addAccountRepository.add(account)
+    const hashedPassword = await this.encrypter.hash(account.password)
+    const newAccount = await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
     return newAccount
   }
 }
