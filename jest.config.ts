7f2a544f2dc2bdcecfa11d59e95f292dee8c4742
Lucas <lucas_99silva@hotmail.com>
2022-07-25 18:13:25 -0300

    fix: add files to the jest ignore

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ export default {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{js,ts}',
     '!**/node_modules/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/**/protocols/**',
     '!**/dist/**'
   ],
   coverageDirectory: 'coverage',
