67ccebba4ee3fb57cb0f3d7ee13e28ae1435f410
Lucas <lucas_99silva@hotmail.com>
2022-07-29 15:18:42 -0300

    chore: add pg-mem

--- src/infra/db/ormconfig.ts
+++ src/infra/db/ormconfig.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm'
 
 export const Db = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
+  host: process.env.POSTGRES_HOST || '127.0.0.1',
   port: Number(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
