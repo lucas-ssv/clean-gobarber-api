456325c889478d9b68ed6cd3051ebdd9e28d216a
Lucas <lucas_99silva@hotmail.com>
2022-07-29 16:46:04 -0300

    chore: add pg-mem

--- src/infra/db/ormconfig.ts
+++ src/infra/db/ormconfig.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm'
 
 export const Db = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST || '127.0.0.1',
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'gobarber',
   synchronize: true,
   logging: true,
   entities: ['src/infra/db/database/entities/**/*.ts'],
