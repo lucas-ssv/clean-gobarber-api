5c55ee740bf598a09a55fb992ce29ffed95a11a8
Lucas <lucas_99silva@hotmail.com>
2022-08-05 14:10:10 -0300

    refactor: move SUT creation to a factory method

--- src/infra/email-validator/email-validator-adapter/email-validator-adapter.spec.ts
+++ src/infra/email-validator/email-validator-adapter/email-validator-adapter.spec.ts
@@ -1,10 +1,12 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
+const makeSut = (): EmailValidatorAdapter => new EmailValidatorAdapter()
+
 describe('EmailValidatorAdapter', () => {
   test('Should call EmailValidatorAdapter with correct value', () => {
+    const sut = makeSut()
     const validatorSpy = jest.spyOn(validator, 'isEmail')
-    const sut = new EmailValidatorAdapter()
     sut.isValid('any_email')
     expect(validatorSpy).toHaveBeenCalledWith('any_email')
   })
