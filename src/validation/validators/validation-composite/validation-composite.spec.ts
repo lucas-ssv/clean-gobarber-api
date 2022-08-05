bdd89698b6a9e4fca07f3fce226dbc886e040626
Lucas <lucas_99silva@hotmail.com>
2022-08-05 16:08:06 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -3,13 +3,19 @@ import { EmailValidatorAdapter } from '../../../infra/email-validator/email-vali
 import { MissingParamError } from '../../../presentation/errors/missing-param-error'
 import { EmailValidation } from '../email-validation/email-validation'
 import { RequiredFieldValidation } from '../required-field-validation/required-field-validation'
+import { Validation } from '../../../presentation/protocols/validation'
+
+const makeSut = (): ValidationComposite => {
+  const validations: Validation[] = [
+    new EmailValidation('any_email', new EmailValidatorAdapter()),
+    new RequiredFieldValidation('any_field')
+  ]
+  return new ValidationComposite(validations)
+}
 
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
-    const sut = new ValidationComposite([
-      new RequiredFieldValidation('any_field'),
-      new EmailValidation('any_email', new EmailValidatorAdapter())
-    ])
+    const sut = makeSut()
     const error = sut.validate({ any_email: 'any_email@mail.com' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
