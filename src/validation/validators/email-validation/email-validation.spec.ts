c196c2f0d72aeec4466baa44838e102076c54916
Lucas <lucas_99silva@hotmail.com>
2022-08-05 11:46:15 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/email-validation/email-validation.spec.ts
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -1,11 +1,25 @@
 import { EmailValidation } from './email-validation'
 import { EmailValidatorStub } from '../../../presentation/test/validation/mock-email-validation'
+import { EmailValidator } from '../../protocols/email-validator'
+
+type SutTypes = {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new EmailValidation('any_email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 
 describe('EmailValidation', () => {
   test('Should call EmailValidator with correct value', () => {
-    const emailValidatorStub = new EmailValidatorStub()
+    const { sut, emailValidatorStub } = makeSut()
     const validSpy = jest.spyOn(emailValidatorStub, 'isValid')
-    const sut = new EmailValidation('any_email', emailValidatorStub)
     sut.validate({ any_email: 'any_value' })
     expect(validSpy).toHaveBeenCalledWith('any_value')
   })
