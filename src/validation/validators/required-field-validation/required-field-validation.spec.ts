3de682b0afd204714b355568de1c33057c5e9ba9
Lucas <lucas_99silva@hotmail.com>
2022-08-05 09:06:01 -0300

    test: ensure RequiredFieldValidation returns null if there is no error

--- src/validation/validators/required-field-validation/required-field-validation.spec.ts
+++ src/validation/validators/required-field-validation/required-field-validation.spec.ts
@@ -7,7 +7,14 @@ describe('RequiredFieldValidation', () => {
   test('Should return MissingParamError if field is empty', () => {
     const fieldName = 'any_field'
     const sut = makeSut(fieldName)
-    const error = sut.validate({ any_field: '' })
+    const error = sut.validate({ [fieldName]: '' })
     expect(error).toEqual(new MissingParamError(fieldName))
+  })
+
+  test('Should return null if there is no error', () => {
+    const fieldName = 'any_field'
+    const sut = makeSut(fieldName)
+    const error = sut.validate({ [fieldName]: 'any_value' })
+    expect(error).toBeNull()
   })
 })
