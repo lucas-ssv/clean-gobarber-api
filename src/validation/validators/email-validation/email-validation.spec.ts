bec921047949a8a70f96129fb4942555acfb6b13
Lucas <lucas_99silva@hotmail.com>
2022-08-05 13:43:47 -0300

    test: ensure EmailValidation returns an error if email is not valid

--- src/validation/validators/email-validation/email-validation.spec.ts
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -1,6 +1,7 @@
 import { EmailValidation } from './email-validation'
 import { EmailValidatorStub } from '../../../presentation/test/validation/mock-email-validation'
 import { EmailValidator } from '../../protocols/email-validator'
+import { InvalidEmailError } from '../../../presentation/errors/invalid-email-error'
 
 type SutTypes = {
   sut: EmailValidation
@@ -22,5 +23,12 @@ describe('EmailValidation', () => {
     const validSpy = jest.spyOn(emailValidatorStub, 'isValid')
     sut.validate({ any_email: 'any_value' })
     expect(validSpy).toHaveBeenCalledWith('any_value')
+  })
+
+  test('Should return an error if email is not valid', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const error = sut.validate({ any_email: 'any_value' })
+    expect(error).toEqual(new InvalidEmailError())
   })
 })
