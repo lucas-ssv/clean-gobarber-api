7cf1deab5e1dadfbe410506af2cf46442b0afc6a
Lucas <lucas_99silva@hotmail.com>
2022-08-08 11:24:44 -0300

    feat: ensure content type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from '../middlewares/body-parser/body-parser'
+import { contentType } from '../middlewares/content-type/content-type'
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser)
+  app.use(contentType)
 }
