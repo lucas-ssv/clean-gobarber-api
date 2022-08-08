2ab159a77bd21614c6ad1aac567e641ad594e2f4
Lucas <lucas_99silva@hotmail.com>
2022-08-08 09:57:09 -0300

    test: ensure body parser as json

---
+++ src/main/middlewares/body-parser/body-parser.test.ts
@@ -0,0 +1,14 @@
+import { app } from '../../config/app'
+import request from 'supertest'
+
+describe('BodyParser', () => {
+  test('Should body parser as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'any_name' })
+      .expect({ name: 'any_name' })
+  })
+})
