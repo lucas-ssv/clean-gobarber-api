826db8900cb4d9e747b34d898a1ac7bf87ceceab
Lucas <lucas_99silva@hotmail.com>
2022-08-08 08:52:17 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,19 +4,30 @@ import { MissingParamError } from '../../../presentation/errors/missing-param-er
 import { EmailValidation } from '../email-validation/email-validation'
 import { RequiredFieldValidation } from '../required-field-validation/required-field-validation'
 import { Validation } from '../../../presentation/protocols/validation'
+import { InvalidEmailError } from '../../../presentation/errors/invalid-email-error'
 
-const makeSut = (): ValidationComposite => {
-  const validations: Validation[] = [
-    new EmailValidation('any_email', new EmailValidatorAdapter()),
-    new RequiredFieldValidation('any_field')
-  ]
+const makeSut = (validations: Validation[]): ValidationComposite => {
   return new ValidationComposite(validations)
 }
 
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const validations: Validation[] = [
+      new EmailValidation('any_email', new EmailValidatorAdapter()),
+      new RequiredFieldValidation('any_field')
+    ]
+    const sut = makeSut(validations)
     const error = sut.validate({ any_email: 'any_email@mail.com' })
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('Should return the first error if more than one validation fails', () => {
+    const validations = [
+      new EmailValidation('any_email', new EmailValidatorAdapter()),
+      new RequiredFieldValidation('any_field')
+    ]
+    const sut = makeSut(validations)
+    const error = sut.validate({ any_email: 'any_email' })
+    expect(error).toEqual(new InvalidEmailError())
   })
 })
