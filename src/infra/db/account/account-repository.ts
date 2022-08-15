528e9b12a957ca043e58bcb80261ecb356b2a68f
Lucas <lucas_99silva@hotmail.com>
2022-08-15 11:53:19 -0300

    feat: ensure AccountRepository returns an account on loadByEmail success

--- src/infra/db/account/account-repository.ts
+++ src/infra/db/account/account-repository.ts
@@ -1,14 +1,21 @@
 import { AddAccountRepository } from '../../../data/protocols/db/add-account-repository'
+import { LoadByEmailRepository } from '../../../data/protocols/db/load-by-email-repository'
 import { AccountModel } from '../../../domain/models/account'
 import { AddAccountParams } from '../../../domain/usecases/add-account'
 import { Account } from '../database/entities/account'
 import { DbHelper } from '../helpers/db-helper'
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadByEmailRepository {
   async add (account: AddAccountParams): Promise<AccountModel> {
     const repo = await DbHelper.getRepository(Account)
     const result = await repo.insert(account as any)
     const accountData = await repo.findOne({ where: { id: result.identifiers[0].id } })
+    return accountData as AccountModel
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const repo = await DbHelper.getRepository(Account)
+    const accountData = await repo.findOne({ where: { email } })
     return accountData as AccountModel
   }
 }
