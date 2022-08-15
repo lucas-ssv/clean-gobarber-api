bf91bcaf71b6b7e4cf8c2bde859134ee6fe12486
Lucas <lucas_99silva@hotmail.com>
2022-08-15 09:41:07 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-by-email/db-load-by-email.spec.ts
+++ src/data/usecases/load-by-email/db-load-by-email.spec.ts
@@ -1,11 +1,25 @@
 import { DbLoadByEmail } from './db-load-by-email'
 import { LoadByEmailRepositoryStub } from '../../tests/mock-load-by-email-repository'
+import { LoadByEmailRepository } from '../../protocols/db/load-by-email-repository'
+
+type SutTypes = {
+  sut: DbLoadByEmail
+  loadAccountByEmailRepositoryStub: LoadByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = new LoadByEmailRepositoryStub()
+  const sut = new DbLoadByEmail(loadAccountByEmailRepositoryStub)
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
+  }
+}
 
 describe('DbLoadByEmail', () => {
   test('Should call LoadAccountByEmailRepository with correct value', async () => {
-    const loadAccountByEmailRepositoryStub = new LoadByEmailRepositoryStub()
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail')
-    const sut = new DbLoadByEmail(loadAccountByEmailRepositoryStub)
     await sut.loadByEmail('any_email@mail.com')
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
