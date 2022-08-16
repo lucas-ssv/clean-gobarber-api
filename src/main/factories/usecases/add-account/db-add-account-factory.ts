f2853942617298dde35567b1e64fab61d207cdd9
Lucas <lucas_99silva@hotmail.com>
2022-08-16 11:19:53 -0300

    feat: add DbLoadByEmail composition

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -1,9 +1,11 @@
 import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account'
+import { DbLoadByEmail } from '../../../../data/usecases/load-by-email/db-load-by-email'
 import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountRepository } from '../../../../infra/db/account/account-repository'
 
 export const makeDbAddAccount = (): DbAddAccount => {
   const accountRepository = new AccountRepository()
   const encrypter = new BcryptAdapter()
-  return new DbAddAccount(accountRepository, encrypter)
+  const loadByEmailRepository = new DbLoadByEmail(accountRepository)
+  return new DbAddAccount(accountRepository, encrypter, loadByEmailRepository)
 }
