53f3ffeff3fde8513b15960efcf94035a06617ec
Lucas <lucas_99silva@hotmail.com>
2022-08-23 15:17:11 -0300

    feat: ensure DbAuthentication calls GenerateToken with correct values

---
+++ src/data/protocols/criptography/generate-token.ts
@@ -0,0 +1,3 @@
+export interface GenerateToken {
+  generate: (value: string) => string
+}
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,17 +2,22 @@ import { AuthAccount } from '../../../domain/models/auth-account'
 import { Authentication } from '../../../domain/usecases/authentication'
 import { LoadByEmail } from '../../../domain/usecases/load-by-email'
 import { Compare } from '../../protocols/criptography/compare'
+import { GenerateToken } from '../../protocols/criptography/generate-token'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadByEmailRepository: LoadByEmail,
-    private readonly hashCompare: Compare
+    private readonly hashCompare: Compare,
+    private readonly generateToken: GenerateToken
   ) {}
 
   async auth (email: string, password: string): Promise<AuthAccount> {
     const account = await this.loadByEmailRepository.loadByEmail(email)
     if (account) {
-      await this.hashCompare.compare(password, account.password)
+      const isValidCompare = await this.hashCompare.compare(password, account.password)
+      if (isValidCompare) {
+        this.generateToken.generate(account.id)
+      }
     }
     return null
   }
