13ca261658a65861c2a0857ab68945aceb7b34d8
Lucas <lucas_99silva@hotmail.com>
2022-08-24 17:46:45 -0300

    refactor: move SUT creation to a factory method

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,9 +2,11 @@ import { JwtAdapter } from './jwt-adapter'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
+const makeSut = (): JwtAdapter => new JwtAdapter()
+
 describe('JwtAdapter', () => {
   test('Should call jwt.sign() with correct values', () => {
-    const sut = new JwtAdapter()
+    const sut = makeSut()
     const jwtSpy = jest.spyOn(jwt, 'sign')
     sut.sign('any_value')
     expect(jwtSpy).toHaveBeenCalledWith({ id: 'any_value' }, process.env.JWT_SECRET, {
