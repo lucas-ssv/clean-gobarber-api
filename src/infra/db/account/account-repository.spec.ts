5e4a9caf40a5594425f71272ac10e2f62e3168e9
Lucas <lucas_99silva@hotmail.com>
2022-08-24 10:46:51 -0300

    test: ensure AccountRepository refreshes the account access token on RefreshToken success

--- src/infra/db/account/account-repository.spec.ts
+++ src/infra/db/account/account-repository.spec.ts
@@ -2,6 +2,7 @@ import { AccountRepository } from './account-repository'
 import { Account } from '../database/entities/account'
 import { connect } from '../../test/mock-pg-mem'
 import { DataSource } from 'typeorm'
+import { DbHelper } from '../helpers/db-helper'
 
 let client: DataSource = null
 
@@ -36,5 +37,21 @@ describe('AccountRepository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeTruthy()
     expect(account.email).toBe('any_email@mail.com')
+  })
+
+  test('Should refresh the account access token on RefreshToken success', async () => {
+    const sut = makeSut()
+    const repo = await DbHelper.getRepository(Account)
+    const result = await repo.insert({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      passwordConfirmation: 'any_password',
+      isBarber: false
+    } as any)
+    await sut.refresh('any_token', result.identifiers[0].id)
+    const account = await repo.findOne({ where: { id: result.identifiers[0].id } })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
   })
 })
