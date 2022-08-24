3ac21c2ed0605e23af727ede1d5e7a0ecdc1265d
Lucas <lucas_99silva@hotmail.com>
2022-08-24 15:55:38 -0300

    test: ensure JwtAdapter calls jwt.sign() with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,14 @@
+import { JwtAdapter } from './jwt-adapter'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+describe('JwtAdapter', () => {
+  test('Should call jwt.sign() with correct values', () => {
+    const sut = new JwtAdapter()
+    const jwtSpy = jest.spyOn(jwt, 'sign')
+    sut.generate('any_value')
+    expect(jwtSpy).toHaveBeenCalledWith({ id: 'any_value' }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    })
+  })
+})
