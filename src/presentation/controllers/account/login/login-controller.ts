61106580312f5307919f89073cea7755577c1d1c
Lucas <lucas_99silva@hotmail.com>
2022-08-29 14:19:34 -0300

    feat: ensure LoginController calls Validation with correct values

--- src/presentation/controllers/account/login/login-controller.ts
+++ src/presentation/controllers/account/login/login-controller.ts
@@ -1,12 +1,17 @@
 import { Authentication } from '../../../../domain/usecases/authentication'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
+import { Validation } from '../../../protocols/validation'
 
 export class LoginController implements Controller {
-  constructor (private readonly authentication: Authentication) {}
+  constructor (
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
+    this.validation.validate(httpRequest.body)
     await this.authentication.auth(email, password)
     return null
   }
