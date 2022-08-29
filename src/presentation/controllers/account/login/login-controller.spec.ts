81e18c7b321ef480b4c6cd6f8680ed72816d2952
Lucas <lucas_99silva@hotmail.com>
2022-08-29 14:09:17 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/controllers/account/login/login-controller.spec.ts
+++ src/presentation/controllers/account/login/login-controller.spec.ts
@@ -1,11 +1,25 @@
 import { LoginController } from './login-controller'
 import { AuthenticationStub } from '../../../test/account/mock-authentication'
+import { Authentication } from '../../../../domain/usecases/authentication'
+
+type SutTypes = {
+  sut: LoginController
+  authenticationStub: Authentication
+}
+
+const makeSut = (): SutTypes => {
+  const authenticationStub = new AuthenticationStub()
+  const sut = new LoginController(authenticationStub)
+  return {
+    sut,
+    authenticationStub
+  }
+}
 
 describe('LoginController', () => {
   test('Should call Authentication with correct values', async () => {
-    const authenticationStub = new AuthenticationStub()
+    const { sut, authenticationStub } = makeSut()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
-    const sut = new LoginController(authenticationStub)
     await sut.handle({
       body: {
         email: 'any_email@mail.com',
