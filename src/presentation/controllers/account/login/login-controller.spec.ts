5b77239dbffe1ffd7bcf5a6c95e9b8a44ed2b46a
Lucas <lucas_99silva@hotmail.com>
2022-08-29 14:28:00 -0300

    test: ensure LoginController returns 400 if Validation returns an error

--- src/presentation/controllers/account/login/login-controller.spec.ts
+++ src/presentation/controllers/account/login/login-controller.spec.ts
@@ -4,6 +4,7 @@ import { Authentication } from '../../../../domain/usecases/authentication'
 import { ValidationStub } from '../../../test/validation/mock-validation'
 import { Validation } from '../../../protocols/validation'
 import { mockLoginRequest } from '../../../test/account/mock-login-request'
+import { badRequest } from '../../../helpers/http/helper'
 
 type SutTypes = {
   sut: LoginController
@@ -36,5 +37,12 @@ describe('LoginController', () => {
     const httpRequest = mockLoginRequest()
     await sut.handle(httpRequest)
     expect(validSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(mockLoginRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
