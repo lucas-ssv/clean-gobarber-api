4675852f7b40e41bf994819bd7be5eddab3278ee
Lucas <lucas_99silva@hotmail.com>
2022-08-30 10:43:41 -0300

    feat: add Authentication to the SignUpController factory

--- src/main/factories/controllers/signup-controller-factory/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory/signup-controller-factory.ts
@@ -1,8 +1,9 @@
 import { SignUpController } from '../../../../presentation/controllers/account/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols/controller'
 import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication'
 import { makeSignUpValidation } from './signup-validations/signup-validations'
 
 export const makeSignUpController = (): Controller => {
-  return new SignUpController(makeDbAddAccount(), makeSignUpValidation())
+  return new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
 }
