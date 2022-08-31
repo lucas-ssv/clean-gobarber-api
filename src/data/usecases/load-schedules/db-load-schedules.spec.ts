1f53049147417878a90810e3d846318dbda08f1b
Lucas <lucas_99silva@hotmail.com>
2022-08-31 18:11:19 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-schedules/db-load-schedules.spec.ts
+++ src/data/usecases/load-schedules/db-load-schedules.spec.ts
@@ -1,11 +1,25 @@
 import { DbLoadSchedules } from './db-load-schedules'
 import { LoadSchedulesRepositoryStub } from '../../tests/db/mock-load-schedules'
+import { LoadSchedulesRepository } from '../../protocols/db/load-schedules-repository'
+
+type SutTypes = {
+  sut: DbLoadSchedules
+  loadSchedulesRepositoryStub: LoadSchedulesRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSchedulesRepositoryStub = new LoadSchedulesRepositoryStub()
+  const sut = new DbLoadSchedules(loadSchedulesRepositoryStub)
+  return {
+    sut,
+    loadSchedulesRepositoryStub
+  }
+}
 
 describe('DbLoadSchedules', () => {
   test('Should call LoadSchedulesRepository with correct value', async () => {
-    const loadSchedulesRepositoryStub = new LoadSchedulesRepositoryStub()
+    const { sut, loadSchedulesRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadSchedulesRepositoryStub, 'load')
-    const sut = new DbLoadSchedules(loadSchedulesRepositoryStub)
     await sut.load('any_account_id')
     expect(loadSpy).toHaveBeenCalledWith('any_account_id')
   })
