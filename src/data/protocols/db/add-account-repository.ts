498ddebb7baac52f1eaa3a70c8fb2793ee937a49
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-12 16:41:29 -0300

    fix: adjust return from AddAccountRepositoryStub to void

--- src/data/protocols/db/add-account-repository.ts
+++ src/data/protocols/db/add-account-repository.ts
@@ -1,6 +1,5 @@
-import { Account } from "../../../domain/models/account";
 import { AccountParams } from "../../../domain/usecases/add-account";
 
 export interface AddAccountRepository {
-  add: (account: AccountParams) => Promise<Account>
+  add: (account: AccountParams) => Promise<void>
 }
