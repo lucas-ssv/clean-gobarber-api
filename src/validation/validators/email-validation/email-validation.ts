83213bf30656cbab0f55a285010ce9d0320777c7
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-13 18:10:13 -0300

    feat: ensure EmailValidation returns null if field is an email

--- src/validation/validators/email-validation/email-validation.ts
+++ src/validation/validators/email-validation/email-validation.ts
@@ -5,6 +5,7 @@ export class EmailValidation implements Validation {
   constructor (private readonly fieldName: string) {}
 
   validate (input: object): Error {
-    return new InvalidEmailError()
+    const isValidEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    return isValidEmail.test(input[this.fieldName]) ? null as any : new InvalidEmailError()
   }
 }
