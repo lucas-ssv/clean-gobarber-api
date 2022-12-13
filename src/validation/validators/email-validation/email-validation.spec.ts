79411b57ece6e208fc2e1553b7c0d4d35c6d16b5
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-13 18:10:32 -0300

    test: ensure EmailValidation returns null if field is an email

--- src/validation/validators/email-validation/email-validation.spec.ts
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -8,4 +8,11 @@ describe('EmailValidation', () => {
     const error = sut.validate({ [fieldName]: 'any_email' })
     expect(error).toEqual(new InvalidEmailError())
   })
+
+  test('Should return null if field is an email', () => {
+    const fieldName = 'any_field'
+    const sut = new EmailValidation(fieldName)
+    const error = sut.validate({ [fieldName]: 'any_email@mail.com' })
+    expect(error).toBeNull()
+  })
 })
