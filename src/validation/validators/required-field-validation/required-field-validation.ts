05a72eb7972fd45d5038752e1710233e7ce0d653
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-13 16:00:56 -0300

    feat: ensure RequiredFieldValidation returns null if field has typeof boolean

--- src/validation/validators/required-field-validation/required-field-validation.ts
+++ src/validation/validators/required-field-validation/required-field-validation.ts
@@ -5,6 +5,9 @@ export class RequiredFieldValidation implements Validation {
   constructor (private readonly fieldName: string) {}
 
   validate (input: object): Error {
+    if (typeof input[this.fieldName] === 'boolean') {
+      return null as any
+    }
     return input[this.fieldName] ? null as any : new RequiredFieldError(this.fieldName)
   }
 }
