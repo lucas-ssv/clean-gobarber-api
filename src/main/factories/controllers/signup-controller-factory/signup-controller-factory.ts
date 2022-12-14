1a01ede1478cecc967f3a4fd672dba26572a53ec
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-14 18:08:34 -0300

    feat: add ValidationComposite in the composition

--- src/main/factories/controllers/signup-controller-factory/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory/signup-controller-factory.ts
@@ -7,16 +7,17 @@ import { Validation } from '../../../../presentation/protocols/validation'
 import { EmailValidation } from '../../../../validation/validators/email-validation/email-validation'
 import { MinLengthValidation } from '../../../../validation/validators/min-length-validation/min-length-validation'
 import { RequiredFieldValidation } from '../../../../validation/validators/required-field-validation/required-field-validation'
+import { ValidationComposite } from '../../../../validation/validators/validation-composite/validation-composite'
 
 export const makeSignUpController = (): Controller => {
-  let validation: Validation = null as any
-  for (const fieldName of ['name', 'email', 'password', 'isBarber']) {
-    validation = new RequiredFieldValidation(fieldName)
+  const validations: Validation[] = []
+  for (const fieldName of ['name', 'email', 'password']) {
+    validations.push(new RequiredFieldValidation(fieldName))
   }
-  validation = new EmailValidation('email')
-  validation = new MinLengthValidation('password', 6)
+  validations.push(new EmailValidation('email'))
+  validations.push(new MinLengthValidation('password', 6))
   const encrypter = new BcryptAdapter()
   const accountRepository = new AccountRepository()
   const addAccount = new DbAddAccount(encrypter, accountRepository)
-  return new SignUpController(validation, addAccount)
+  return new SignUpController(new ValidationComposite(validations), addAccount)
 }
