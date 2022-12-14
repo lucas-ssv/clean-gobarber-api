34f7030564c0ce5d9cf6b6b2eaa4fadc3500be83
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-14 10:52:27 -0300

    test: ensure MinLengthValidation returns null if field is equal or greater than 6

--- src/validation/validators/min-length-validation/min-length-validation.spec.ts
+++ src/validation/validators/min-length-validation/min-length-validation.spec.ts
@@ -9,4 +9,12 @@ describe('MinLengthValidation', () => {
     const error = sut.validate({ [fieldName]: 'any' })
     expect(error).toEqual(new MinLengthFieldError(fieldName, length))
   })
+
+  test('Should return null if field is equal or greater than 6', () => {
+    const fieldName = 'any_field'
+    const length = 6
+    const sut = new MinLengthValidation(fieldName, length)
+    const error = sut.validate({ [fieldName]: 'any_field' })
+    expect(error).toBeNull()
+  })
 })
