6a6f2bfb8d79760fb037ada690c19657b4ba8366
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-14 16:06:28 -0300

    feat: add setup routes

---
+++ src/main/config/routes.ts
@@ -0,0 +1,14 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import path from 'path'
+
+export const setupRoutes = (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  readdirSync(path.resolve(__dirname, '..', 'routes')).map(async file => {
+    if (!file.endsWith('.map')) {
+      const route = await import(`../routes/${file}`)
+      route.default(router)
+    }
+  })
+}
