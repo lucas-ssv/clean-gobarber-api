8cd955d3a49b98da357b20af30a07d067b4ce3b5
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-14 17:20:38 -0300

    feat: add signup route

---
+++ src/main/routes/signup.ts
@@ -0,0 +1,7 @@
+import { makeSignUpController } from '../factories/controllers/signup-controller-factory/signup-controller-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.post('/signup', adaptRoute(makeSignUpController()))
+}
