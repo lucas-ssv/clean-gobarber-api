1c86f8321898291dd14c7c9fff53adab285a87ed
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-15 14:19:50 -0300

    test: ensure AccountRepository returns an account on load by email success

--- src/infra/db/account/account-repository.spec.ts
+++ src/infra/db/account/account-repository.spec.ts
@@ -6,19 +6,38 @@ describe('AccountRepository', () => {
     await client.account.deleteMany()
   })
 
-  test('Should add an account on add success', async () => {
-    const mockAccount = {
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'hashed_password',
-      isBarber: false
-    }
-    const sut = new AccountRepository()
-    const account = await sut.add(mockAccount)
-    expect(account).toBeTruthy()
-    expect(mockAccount.name).toBe(account?.name)
-    expect(mockAccount.email).toBe(account?.email)
-    expect(mockAccount.password).toBe(account?.password)
-    expect(mockAccount.isBarber).toBe(account?.isBarber)
+  describe('add()', () => {
+    test('Should add an account on add success', async () => {
+      const mockAccount = {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'hashed_password',
+        isBarber: false
+      }
+      const sut = new AccountRepository()
+      const account = await sut.add(mockAccount)
+      expect(account).toBeTruthy()
+      expect(mockAccount.name).toBe(account?.name)
+      expect(mockAccount.email).toBe(account?.email)
+      expect(mockAccount.password).toBe(account?.password)
+      expect(mockAccount.isBarber).toBe(account?.isBarber)
+    })
+  })
+
+  describe('loadByEmail()', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      const mockEmail = 'any_email@mail.com'
+      const sut = new AccountRepository()
+      await client.account.create({
+        data: {
+          name: 'any_name',
+          email: mockEmail,
+          password: 'any_password'
+        }
+      })
+      const accountByEmail = await sut.loadByEmail(mockEmail)
+      expect(accountByEmail).toBeTruthy()
+      expect(accountByEmail.email).toBe(mockEmail)
+    })
   })
 })
