bec315228e6ea2bd8ab66f51e7e84998d89f4566
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-15 14:19:28 -0300

    feat: ensure AccountRepository returns an account on load by email success

--- src/infra/db/account/account-repository.ts
+++ src/infra/db/account/account-repository.ts
@@ -1,7 +1,9 @@
 import { AddAccountRepository } from '../../../data/protocols/db/add-account-repository'
+import { LoadByEmailRepository } from '../../../data/protocols/db/load-by-email-repository'
+import { Account } from '../../../domain/models/account'
 import { client } from '../client'
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadByEmailRepository {
   async add (account: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     const accountData = await client.account.create({
       data: {
@@ -13,5 +15,14 @@ export class AccountRepository implements AddAccountRepository {
     })
     const result = { ...accountData, isBarber: accountData.is_barber }
     return result
+  }
+
+  async loadByEmail (email: string): Promise<Account> {
+    const accountByEmail = await client.account.findUnique({
+      where: {
+        email
+      }
+    })
+    return accountByEmail as any
   }
 }
