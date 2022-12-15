e199e242af9c5d5e68e815e59354a7d43d7a49b0
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-15 15:47:09 -0300

    test: ensure DbLoadByEmail returns null if no account found

--- src/data/usecases/load-by-email/db-load-by-email.spec.ts
+++ src/data/usecases/load-by-email/db-load-by-email.spec.ts
@@ -1,12 +1,18 @@
 import { DbLoadByEmail } from './db-load-by-email'
-import { LoadByEmailRepositoryStub } from '../../tests/db/mock-load-by-email-repository'
 import { LoadByEmail } from '../../../domain/usecases/load-by-email'
 import { Account } from '../../../domain/models/account'
 import { LoadByEmailRepository } from '../../protocols/db/load-by-email-repository'
+import { mockAccount } from '../../tests/db/mock-add-account-repository'
 
 type SutTypes = {
   sut: LoadByEmail<Account>
   loadByEmailRepositoryStub: LoadByEmailRepository<Account>
+}
+
+class LoadByEmailRepositoryStub implements LoadByEmailRepository<Account> {
+  async loadByEmail (email: string): Promise<Account> {
+    return mockAccount()
+  }
 }
 
 const makeSut = (): SutTypes => {
@@ -24,5 +30,12 @@ describe('DbLoadByEmail usecase', () => {
     const loadSpy = jest.spyOn(loadByEmailRepositoryStub, 'loadByEmail')
     await sut.loadByEmail('any_email@mail.com')
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Should return null if no account found', async () => {
+    const { sut, loadByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(Promise.resolve(null) as any)
+    const accountByEmail = await sut.loadByEmail('any_email@mail.com')
+    expect(accountByEmail).toBeNull()
   })
 })
