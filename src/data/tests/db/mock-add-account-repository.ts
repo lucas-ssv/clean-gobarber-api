7259d24c50606f1ccbafeee9b39ad2de47edd1a1
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-15 11:21:11 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/tests/db/mock-add-account-repository.ts
+++ src/data/tests/db/mock-add-account-repository.ts
@@ -1,5 +1,6 @@
-import { AccountParams } from "../../../domain/usecases/add-account";
-import { AddAccountRepository } from "../../protocols/db/add-account-repository";
+import { Account } from '../../../domain/models/account'
+import { AccountParams } from '../../../domain/usecases/add-account'
+import { AddAccountRepository } from '../../protocols/db/add-account-repository'
 
 export const mockAccountParams = (): AccountParams => ({
   name: 'any_name',
@@ -8,8 +9,16 @@ export const mockAccountParams = (): AccountParams => ({
   isBarber: false
 })
 
+export const mockAccount = (): Account => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+  isBarber: false
+})
+
 export class AddAccountRepositoryStub implements AddAccountRepository {
-  async add (account: AccountParams): Promise<void> {
-    return await Promise.resolve(null) as any
+  async add (account: AccountParams): Promise<Account> {
+    return mockAccount()
   }
 }
