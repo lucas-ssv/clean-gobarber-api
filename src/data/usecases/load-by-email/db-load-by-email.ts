8fabd81a39321e4a87aed54dca1655b7f8501d1e
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-15 14:42:29 -0300

    feat: ensure DbLoadByEmail calls LoadByEmailRepository with correct value

---
+++ src/data/usecases/load-by-email/db-load-by-email.ts
@@ -0,0 +1,12 @@
+import { Account } from '../../../domain/models/account'
+import { LoadByEmail } from '../../../domain/usecases/load-by-email'
+import { LoadByEmailRepository } from '../../protocols/db/load-by-email-repository'
+
+export class DbLoadByEmail implements LoadByEmail<Account> {
+  constructor (private readonly loadByEmailRepository: LoadByEmailRepository) {}
+
+  async loadByEmail (email: string): Promise<Account> {
+    await this.loadByEmailRepository.loadByEmail(email)
+    return null as any
+  }
+}
