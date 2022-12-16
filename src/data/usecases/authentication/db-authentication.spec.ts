6b05ec23332315c307f5f55157635140ed47e5b6
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-16 09:36:44 -0300

    test: ensure DbAuthentication returns null if there is no account

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,11 +1,17 @@
 import { DbAuthentication } from './db-authentication'
-import { LoadByEmailRepositoryStub } from '../../tests/db/mock-load-by-email-repository'
 import { LoadByEmailRepository } from '../../protocols/db/load-by-email-repository'
 import { Account } from '../../../domain/models/account'
+import { mockAccount } from '../../tests/db/mock-add-account-repository'
 
 type SutTypes = {
   sut: DbAuthentication
   loadByEmailRepositoryStub: LoadByEmailRepository<Account>
+}
+
+class LoadByEmailRepositoryStub implements LoadByEmailRepository<Account> {
+  async loadByEmail (email: string): Promise<Account> {
+    return mockAccount()
+  }
 }
 
 const makeSut = (): SutTypes => {
@@ -17,11 +23,18 @@ const makeSut = (): SutTypes => {
   }
 }
 
-describe('DbAuthentication', () => {
+describe('DbAuthentication usecase', () => {
   test('Should call LoadByEmailRepository with correct value', async () => {
     const { sut, loadByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadByEmailRepositoryStub, 'loadByEmail')
     await sut.auth('any_email@mail.com', 'any_password')
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Should return null if there is no account', async () => {
+    const { sut, loadByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(Promise.resolve(null) as any)
+    const account = await sut.auth('any_email@mail.com', 'any_password')
+    expect(account).toBeNull()
   })
 })
