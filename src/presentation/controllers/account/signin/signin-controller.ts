3d789359489960cf7b3be0a7913722a57e6f7d98
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-19 15:08:16 -0300

    feat: ensure SignInController calls LoadByEmail with correct value

--- src/presentation/controllers/account/signin/signin-controller.ts
+++ src/presentation/controllers/account/signin/signin-controller.ts
@@ -1,16 +1,23 @@
+import { Account } from '../../../../domain/models/account'
+import { LoadByEmail } from '../../../../domain/usecases/load-by-email'
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validation'
 
 export class SignInController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly loadByEmail: LoadByEmail<Account>
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
       return badRequest(error)
     }
+    const { email } = httpRequest.body
+    await this.loadByEmail.loadByEmail(email)
     return await Promise.resolve(null) as any
   }
 }
