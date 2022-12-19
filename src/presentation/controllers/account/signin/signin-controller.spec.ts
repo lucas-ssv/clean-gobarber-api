9b27906a69a26c05fbcb5abe7790f1ce9726d752
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-19 14:49:39 -0300

    test: ensure SignInController returns an error if any validation fails

--- src/presentation/controllers/account/signin/signin-controller.spec.ts
+++ src/presentation/controllers/account/signin/signin-controller.spec.ts
@@ -2,6 +2,7 @@ import { SignInController } from './signin-controller'
 import { mockSignInRequest } from '../../../tests/account/mock-add-account'
 import { ValidationStub } from '../../../tests/mock-validation'
 import { Validation } from '../../../protocols/validation'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 type SutTypes = {
   sut: SignInController,
@@ -23,5 +24,12 @@ describe('SignInController', () => {
     const validationSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(mockSignInRequest())
     expect(validationSpy).toHaveBeenCalledWith(mockSignInRequest().body)
+  })
+
+  test('Should return an error if any validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(mockSignInRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
