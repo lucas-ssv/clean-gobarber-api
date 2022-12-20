6b88c6cafc4d5972a61886a9cfda0192ac704f60
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-20 11:45:50 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-scheduled-times/db-load-scheduled-times.spec.ts
+++ src/data/usecases/load-scheduled-times/db-load-scheduled-times.spec.ts
@@ -1,11 +1,25 @@
 import { DbLoadScheduledTimes } from './db-load-scheduled-times'
 import { LoadScheduledTimesRepositoryStub } from '../../tests/db/mock-load-scheduled-times-repository'
+import { LoadScheduledTimesRepository } from '../../protocols/db/load-scheduled-times-repository'
+
+type SutTypes = {
+  sut: DbLoadScheduledTimes
+  loadScheduledTimesRepositoryStub: LoadScheduledTimesRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadScheduledTimesRepositoryStub = new LoadScheduledTimesRepositoryStub()
+  const sut = new DbLoadScheduledTimes(loadScheduledTimesRepositoryStub)
+  return {
+    sut,
+    loadScheduledTimesRepositoryStub
+  }
+}
 
 describe('DbLoadScheduledTimes usecase', () => {
   test('Should call LoadScheduledTimesRepository', async () => {
-    const loadScheduledTimesRepositoryStub = new LoadScheduledTimesRepositoryStub()
+    const { sut, loadScheduledTimesRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadScheduledTimesRepositoryStub, 'loadAll')
-    const sut = new DbLoadScheduledTimes(loadScheduledTimesRepositoryStub)
     await sut.loadAll()
     expect(loadSpy).toHaveBeenCalled()
   })
