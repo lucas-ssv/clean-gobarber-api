e96f2e154e1266f7bb5383d33f73cf8e6e913e30
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-20 08:38:35 -0300

    test: ensure SignInController returns 404 if Authentication returns null

--- src/presentation/controllers/account/signin/signin-controller.spec.ts
+++ src/presentation/controllers/account/signin/signin-controller.spec.ts
@@ -2,9 +2,10 @@ import { SignInController } from './signin-controller'
 import { mockSignInRequest } from '../../../tests/account/mock-add-account'
 import { ValidationStub } from '../../../tests/mock-validation'
 import { Validation } from '../../../protocols/validation'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, notFound } from '../../../helpers/http/http-helper'
 import { AuthenticationStub } from '../../../tests/account/mock-authentication'
 import { Authentication } from '../../../../domain/usecases/authentication'
+import { InvalidAccountError } from '../../../errors/invalid-account-error'
 
 type SutTypes = {
   sut: SignInController,
@@ -43,5 +44,12 @@ describe('SignInController', () => {
     const authSpy = jest.spyOn(authenticationStub, 'auth')
     await sut.handle(mockSignInRequest())
     expect(authSpy).toHaveBeenCalledWith(mockSignInRequest().body.email, mockSignInRequest().body.password)
+  })
+
+  test('Should return 404 if Authentication returns null', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(Promise.resolve(null) as any)
+    const httpResponse = await sut.handle(mockSignInRequest())
+    expect(httpResponse).toEqual(notFound(new InvalidAccountError()))
   })
 })
