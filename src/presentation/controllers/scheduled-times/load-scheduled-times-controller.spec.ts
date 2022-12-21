18478c6af0d5b04ed0d5eaae1f340bd9e8a0d7dd
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-21 10:27:15 -0300

    test: ensure LoadScheduledTimesController returns 200 if return a list of scheduled times

--- src/presentation/controllers/scheduled-times/load-scheduled-times-controller.spec.ts
+++ src/presentation/controllers/scheduled-times/load-scheduled-times-controller.spec.ts
@@ -1,3 +1,5 @@
+import { mockScheduledTimes } from '../../../domain/tests/scheduled-times/mock-scheduled-times'
+import { ok } from '../../helpers/http/http-helper'
 import { LoadScheduledTimesStub } from '../../tests/scheduled-times/mock-load-scheduled-times'
 import { LoadScheduledTimesController } from './load-scheduled-times-controller'
 
@@ -8,5 +10,12 @@ describe('LoadScheduledTimesController', () => {
     const sut = new LoadScheduledTimesController(loadScheduledTimesStub)
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 if return a list of scheduled times', async () => {
+    const loadScheduledTimesStub = new LoadScheduledTimesStub()
+    const sut = new LoadScheduledTimesController(loadScheduledTimesStub)
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(ok(mockScheduledTimes()))
   })
 })
