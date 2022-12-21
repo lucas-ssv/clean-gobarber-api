8e009ef02c65a516c65c1345528f6ecc7b28bcbd
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-21 10:33:08 -0300

    feat: ensure LoadScheduledTimesController returns 500 if LoadScheduledTimes throws

--- src/presentation/controllers/scheduled-times/load-scheduled-times-controller.ts
+++ src/presentation/controllers/scheduled-times/load-scheduled-times-controller.ts
@@ -1,5 +1,5 @@
 import { LoadScheduledTimes } from '../../../domain/usecases/load-scheduled-times'
-import { ok } from '../../helpers/http/http-helper'
+import { ok, serverError } from '../../helpers/http/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
@@ -7,7 +7,11 @@ export class LoadScheduledTimesController implements Controller {
   constructor (private readonly loadScheduledTimes: LoadScheduledTimes) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const scheduledTimes = await this.loadScheduledTimes.loadAll()
-    return ok(scheduledTimes)
+    try {
+      const scheduledTimes = await this.loadScheduledTimes.loadAll()
+      return ok(scheduledTimes)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
