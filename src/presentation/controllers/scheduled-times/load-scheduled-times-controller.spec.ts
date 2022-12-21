e8b9f732fe1619ac8c682146f4da852d8e8a1f50
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-21 10:29:41 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/controllers/scheduled-times/load-scheduled-times-controller.spec.ts
+++ src/presentation/controllers/scheduled-times/load-scheduled-times-controller.spec.ts
@@ -1,20 +1,33 @@
 import { mockScheduledTimes } from '../../../domain/tests/scheduled-times/mock-scheduled-times'
+import { LoadScheduledTimes } from '../../../domain/usecases/load-scheduled-times'
 import { ok } from '../../helpers/http/http-helper'
 import { LoadScheduledTimesStub } from '../../tests/scheduled-times/mock-load-scheduled-times'
 import { LoadScheduledTimesController } from './load-scheduled-times-controller'
 
+type SutTypes = {
+  sut: LoadScheduledTimesController
+  loadScheduledTimesStub: LoadScheduledTimes
+}
+
+const makeSut = (): SutTypes => {
+  const loadScheduledTimesStub = new LoadScheduledTimesStub()
+  const sut = new LoadScheduledTimesController(loadScheduledTimesStub)
+  return {
+    sut,
+    loadScheduledTimesStub
+  }
+}
+
 describe('LoadScheduledTimesController', () => {
   test('Should call LoadScheduledTimes', async () => {
-    const loadScheduledTimesStub = new LoadScheduledTimesStub()
+    const { sut, loadScheduledTimesStub } = makeSut()
     const loadSpy = jest.spyOn(loadScheduledTimesStub, 'loadAll')
-    const sut = new LoadScheduledTimesController(loadScheduledTimesStub)
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
   })
 
   test('Should return 200 if return a list of scheduled times', async () => {
-    const loadScheduledTimesStub = new LoadScheduledTimesStub()
-    const sut = new LoadScheduledTimesController(loadScheduledTimesStub)
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(mockScheduledTimes()))
   })
