e9faee0a4e294fe6e0ad2c3c9e2fb7a9697e450f
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-22 17:03:30 -0300

    feat: ensure DbUpdateAccount calls Compare with correct values

--- src/data/usecases/update-account/db-update-account.ts
+++ src/data/usecases/update-account/db-update-account.ts
@@ -1,11 +1,18 @@
 import { Account } from '../../../domain/models/account'
 import { UpdateAccount } from '../../../domain/usecases/update-account'
+import { Compare } from '../../protocols/criptography/compare'
 import { LoadByEmailRepository } from '../../protocols/db/load-by-email-repository'
 
 export class DbUpdateAccount implements UpdateAccount {
-  constructor (private readonly loadByEmailRepository: LoadByEmailRepository<Account>) {}
+  constructor (
+    private readonly loadByEmailRepository: LoadByEmailRepository<Account>,
+    private readonly compare: Compare
+  ) {}
 
   async update (params: UpdateAccount.Params): Promise<void> {
-    await this.loadByEmailRepository.loadByEmail(params.email)
+    const account = await this.loadByEmailRepository.loadByEmail(params.email)
+    if (account) {
+      await this.compare.compare(params.currentPassword, account.password)
+    }
   }
 }
