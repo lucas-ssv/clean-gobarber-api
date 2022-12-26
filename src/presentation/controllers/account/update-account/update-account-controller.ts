e986990f63f9810ade9882b9e5f86e3b590d64c4
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-26 11:37:55 -0300

    feat: ensure UpdateAccountController calls UpdateAccount with correct values

--- src/presentation/controllers/account/update-account/update-account-controller.ts
+++ src/presentation/controllers/account/update-account/update-account-controller.ts
@@ -1,16 +1,22 @@
+import { UpdateAccount } from '../../../../domain/usecases/update-account'
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validation'
 
 export class UpdateAccountController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly updateAccount: UpdateAccount
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
+    const request = httpRequest.body
+    const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.updateAccount.update(request)
     return null as any
   }
 }
