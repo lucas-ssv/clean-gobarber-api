ee8a664712e322439a26c5c1d5eedf088e72931c
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-26 11:28:34 -0300

    test: ensure UpdateAccountController returns 400 if any validation fails

--- src/presentation/controllers/account/update-account/update-account-controller.spec.ts
+++ src/presentation/controllers/account/update-account/update-account-controller.spec.ts
@@ -2,6 +2,7 @@ import { UpdateAccountController } from './update-account-controller'
 import { ValidationStub } from '../../../tests/mock-validation'
 import { mockHttpRequestUpdate } from '../../../tests/account/mock-update-account'
 import { Validation } from '../../../protocols/validation'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 type SutTypes = {
   sut: UpdateAccountController
@@ -23,5 +24,12 @@ describe('UpdateAccountController', () => {
     const validationSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(mockHttpRequestUpdate())
     expect(validationSpy).toHaveBeenCalledWith(mockHttpRequestUpdate().body)
+  })
+
+  test('Should return 400 if any validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(mockHttpRequestUpdate())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
