e8ba61d338d5f387c5bc98e01ba066a3f984c0b0
Lucas Vieira <lucas_99silva@hotmail.com>
2022-12-31 15:41:53 -0300

    feat: ensure DbAddAvatar calls UpdateAccountRepository with correct values

--- src/data/usecases/add-avatar/db-add-avatar.ts
+++ src/data/usecases/add-avatar/db-add-avatar.ts
@@ -3,11 +3,13 @@ import { Avatar } from '../../../domain/models/avatar'
 import { AddAvatar } from '../../../domain/usecases/add-avatar'
 import { AddAvatarRepository } from '../../protocols/db/add-avatar-repository'
 import { LoadByEmailRepository } from '../../protocols/db/load-by-email-repository'
+import { UpdateAccountRepository } from '../../protocols/db/update-account-repository'
 
 export class DbAddAvatar implements AddAvatar {
   constructor (
     private readonly loadByEmailRepository: LoadByEmailRepository<Account>,
-    private readonly addAvatarRepository: AddAvatarRepository
+    private readonly addAvatarRepository: AddAvatarRepository,
+    private readonly updateAccountRepository: UpdateAccountRepository
   ) {}
 
   async add (params: AddAvatar.Params): Promise<Avatar> {
@@ -17,6 +19,7 @@ export class DbAddAvatar implements AddAvatar {
         name: params.name,
         url: params.url
       })
+      await this.updateAccountRepository.update({ email: params.email, avatarId: avatar.id })
       return avatar
     }
     return null as any
