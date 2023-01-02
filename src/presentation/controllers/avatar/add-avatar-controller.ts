bdf1b507d6ac3608be0ea4917b68f1a653fc9e6a
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-02 10:26:01 -0300

    feat: ensure AddAvatarController calls AddAvatar with correct values

--- src/presentation/controllers/avatar/add-avatar-controller.ts
+++ src/presentation/controllers/avatar/add-avatar-controller.ts
@@ -1,16 +1,22 @@
+import { AddAvatar } from '../../../domain/usecases/add-avatar'
 import { badRequest } from '../../helpers/http/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Validation } from '../../protocols/validation'
 
 export class AddAvatarController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addAvatar: AddAvatar
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.file)
+    const request = httpRequest.file
+    const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.addAvatar.add(request)
     return await Promise.resolve(null) as any
   }
 }
