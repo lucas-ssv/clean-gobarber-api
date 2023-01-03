661149b5739a655ac07264e34b3e1e1f698c8f79
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-03 14:41:49 -0300

    feat: ensure AccountRepository loads account on load success

--- src/infra/db/account/account-repository.ts
+++ src/infra/db/account/account-repository.ts
@@ -1,10 +1,12 @@
 import { AddAccountRepository } from '../../../data/protocols/db/add-account-repository'
+import { LoadAccountRepository } from '../../../data/protocols/db/load-account-repository'
 import { LoadByEmailRepository } from '../../../data/protocols/db/load-by-email-repository'
 import { UpdateAccountRepository } from '../../../data/protocols/db/update-account-repository'
 import { Account } from '../../../domain/models/account'
+import { LoadAccount } from '../../../domain/usecases/load-account'
 import { client } from '../client'
 
-export class AccountRepository implements AddAccountRepository, LoadByEmailRepository<Account>, UpdateAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadAccountRepository, LoadByEmailRepository<Account>, UpdateAccountRepository {
   async add (account: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     const accountData = await client.account.create({
       data: {
@@ -16,6 +18,15 @@ export class AccountRepository implements AddAccountRepository, LoadByEmailRepos
     })
     const result = { ...accountData, isBarber: accountData.is_barber }
     return result
+  }
+
+  async load (id: string): Promise<LoadAccount.Result> {
+    const account = await client.account.findUnique({
+      where: {
+        id
+      }
+    })
+    return account as any
   }
 
   async loadByEmail (email: string): Promise<Account> {
