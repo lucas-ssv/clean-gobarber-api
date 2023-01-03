0c1e2b24d699b0bb29d25fcf4728b1ecff19641e
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-03 10:55:54 -0300

    feat: ensure LoadAccountController calls Validation with correct values

---
+++ src/presentation/controllers/account/load-account/load-account-controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '../../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../../protocols/http'
+import { Validation } from '../../../protocols/validation'
+
+export class LoadAccountController implements Controller {
+  constructor (private readonly validation: Validation) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
+    return await Promise.resolve(null) as any
+  }
+}
