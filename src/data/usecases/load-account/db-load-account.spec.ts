770615c4253af3129ba3a948b44e4af53ff4079b
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-03 14:25:33 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-account/db-load-account.spec.ts
+++ src/data/usecases/load-account/db-load-account.spec.ts
@@ -1,11 +1,25 @@
 import { DbLoadAccount } from './db-load-account'
 import { LoadAccountRepositoryStub } from '../../tests/db/mock-load-account-repository'
+import { LoadAccountRepository } from '../../protocols/db/load-account-repository'
+
+type SutTypes = {
+  sut: DbLoadAccount
+  loadAccountRepositoryStub: LoadAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountRepositoryStub = new LoadAccountRepositoryStub()
+  const sut = new DbLoadAccount(loadAccountRepositoryStub)
+  return {
+    sut,
+    loadAccountRepositoryStub
+  }
+}
 
 describe('DbLoadAccount usecase', () => {
   test('Should call LoadAccountRepository with correct value', async () => {
-    const loadAccountRepositoryStub = new LoadAccountRepositoryStub()
+    const { sut, loadAccountRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountRepositoryStub, 'load')
-    const sut = new DbLoadAccount(loadAccountRepositoryStub)
     await sut.load('any_id')
     expect(loadSpy).toHaveBeenCalledWith('any_id')
   })
