03b740d60bf39eea388de0bcc9c1b338ebf6afb1
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-03 14:59:00 -0300

    fix: adjust LoadAccountController composition

--- src/main/factories/controllers/load-account-controller-factory/load-account-controller-factory.ts
+++ src/main/factories/controllers/load-account-controller-factory/load-account-controller-factory.ts
@@ -1,8 +1,7 @@
 import { LoadAccountController } from '../../../../presentation/controllers/account/load-account/load-account-controller'
 import { Controller } from '../../../../presentation/protocols/controller'
 import { makeLoadAccount } from '../../usecases/load-account/db-load-account-factory'
-import { makeLoadAccountValidations } from './load-account-validations'
 
 export const makeLoadAccountController = (): Controller => {
-  return new LoadAccountController(makeLoadAccountValidations(), makeLoadAccount())
+  return new LoadAccountController(makeLoadAccount())
 }
--- src/main/factories/usecases/load-account/db-load-account-factory.ts
+++ src/main/factories/usecases/load-account/db-load-account-factory.ts
@@ -1,9 +1,8 @@
-import { DbLoadByEmail } from '../../../../data/usecases/load-by-email/db-load-by-email'
-import { Account } from '../../../../domain/models/account'
-import { LoadByEmail } from '../../../../domain/usecases/load-by-email'
+import { DbLoadAccount } from '../../../../data/usecases/load-account/db-load-account'
+import { LoadAccount } from '../../../../domain/usecases/load-account'
 import { AccountRepository } from '../../../../infra/db/account/account-repository'
 
-export const makeLoadAccount = (): LoadByEmail<Account> => {
+export const makeLoadAccount = (): LoadAccount => {
   const accountRepository = new AccountRepository()
-  return new DbLoadByEmail(accountRepository)
+  return new DbLoadAccount(accountRepository)
 }
