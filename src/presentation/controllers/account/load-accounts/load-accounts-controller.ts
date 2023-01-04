7d77facb61916d78b5d6286470ca99ef03eda149
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-04 15:18:56 -0300

    feat: ensure LoadAccountsController calls LoadAccounts with correct value

--- src/presentation/controllers/account/load-accounts/load-accounts-controller.ts
+++ src/presentation/controllers/account/load-accounts/load-accounts-controller.ts
@@ -1,16 +1,22 @@
+import { LoadAccounts } from '../../../../domain/usecases/load-accounts'
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validation'
 
 export class LoadAccountsController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly loadAccounts: LoadAccounts
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
+    const request = httpRequest.body
+    const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.loadAccounts.loadAll(request)
     return await Promise.resolve(null) as any
   }
 }
