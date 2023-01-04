d32845b424ef8b064402e9ebaa999556477cc0e4
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-04 16:14:53 -0300

    test: ensure TypeFieldValidation returns null if type field satisfies the provided

--- src/validation/validators/type-field-validation/type-field-validation.spec.ts
+++ src/validation/validators/type-field-validation/type-field-validation.spec.ts
@@ -6,7 +6,15 @@ describe('TypeFieldValidation', () => {
     const fieldName = 'any_field'
     const fieldType = 'boolean'
     const sut = new TypeFieldValidation(fieldName, fieldType)
-    const error = sut.validate({ any_field: 'any_value' })
+    const error = sut.validate({ [fieldName]: 'any_value' })
     expect(error).toEqual(new TypeFieldError(fieldName, fieldType))
+  })
+
+  test('Should return null if type field satisfies the provided', () => {
+    const fieldName = 'any_field'
+    const fieldType = 'boolean'
+    const sut = new TypeFieldValidation(fieldName, fieldType)
+    const error = sut.validate({ [fieldName]: false })
+    expect(error).toBeNull()
   })
 })
