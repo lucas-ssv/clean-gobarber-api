f971a1d53446c516c81c0fd7f07e40e4465f3242
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-04 14:35:01 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-accounts/db-load-accounts.spec.ts
+++ src/data/usecases/load-accounts/db-load-accounts.spec.ts
@@ -1,11 +1,25 @@
 import { DbLoadAccounts } from './db-load-accounts'
 import { LoadAccountsRepositoryStub } from '../../tests/db/mock-load-accounts-repository'
+import { LoadAccountsRepository } from '../../protocols/db/load-accounts-repository'
+
+type SutTypes = {
+  sut: DbLoadAccounts
+  loadAccountsRepositoryStub: LoadAccountsRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountsRepositoryStub = new LoadAccountsRepositoryStub()
+  const sut = new DbLoadAccounts(loadAccountsRepositoryStub)
+  return {
+    sut,
+    loadAccountsRepositoryStub
+  }
+}
 
 describe('DbLoadAccounts usecase', () => {
   test('Should call LoadAccountsRepository with correct value', async () => {
-    const loadAccountsRepositoryStub = new LoadAccountsRepositoryStub()
+    const { sut, loadAccountsRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountsRepositoryStub, 'loadAll')
-    const sut = new DbLoadAccounts(loadAccountsRepositoryStub)
     await sut.loadAll({ isBarber: true })
     expect(loadSpy).toHaveBeenCalledWith({ isBarber: true })
   })
