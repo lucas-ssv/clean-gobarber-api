f897a1d71b512c4c3d5c5ac08888025e80de74a2
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-04 15:07:02 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/controllers/account/load-accounts/load-accounts-controller.spec.ts
+++ src/presentation/controllers/account/load-accounts/load-accounts-controller.spec.ts
@@ -1,11 +1,25 @@
 import { LoadAccountsController } from './load-accounts-controller'
 import { ValidationStub } from '../../../tests/mock-validation'
+import { Validation } from '../../../protocols/validation'
+
+type SutTypes = {
+  sut: LoadAccountsController
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = new ValidationStub()
+  const sut = new LoadAccountsController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
 
 describe('LoadAccountsController', () => {
   test('Should call Validation with correct value', async () => {
-    const validationStub = new ValidationStub()
+    const { sut, validationStub } = makeSut()
     const validationSpy = jest.spyOn(validationStub, 'validate')
-    const sut = new LoadAccountsController(validationStub)
     await sut.handle({
       body: {
         isBarber: true
