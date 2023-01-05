143e032fa4ff251bc0f37175972343fa4f61e71b
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-05 11:19:08 -0300

    test: ensure DbAddScheduledTimes throws if LoadAccountRepository throws

--- src/data/usecases/add-scheduled-times/db-add-scheduled-times.spec.ts
+++ src/data/usecases/add-scheduled-times/db-add-scheduled-times.spec.ts
@@ -24,4 +24,13 @@ describe('DbAddScheduledTimes usecase', () => {
     await sut.add(mockAddScheduledTimesParams())
     expect(loadSpy).toHaveBeenCalledWith('any_account_id')
   })
+
+  test('Should throw if LoadAccountRepository throws', async () => {
+    const { sut, loadAccountRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountRepositoryStub, 'load').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.add(mockAddScheduledTimesParams())
+    await expect(promise).rejects.toThrow()
+  })
 })
