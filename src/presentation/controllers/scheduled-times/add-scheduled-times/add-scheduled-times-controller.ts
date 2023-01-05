2669849b188e29030c3a17642d17c643d93dc42d
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-05 14:35:32 -0300

    feat: ensure AddScheduledTimesController calls AddScheduledTimes with correct values

--- src/presentation/controllers/scheduled-times/add-scheduled-times/add-scheduled-times-controller.ts
+++ src/presentation/controllers/scheduled-times/add-scheduled-times/add-scheduled-times-controller.ts
@@ -1,16 +1,22 @@
+import { AddScheduledTimes } from '../../../../domain/usecases/add-scheduled-times'
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validation'
 
 export class AddScheduledTimesController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addScheduledTimes: AddScheduledTimes
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
+    const request = httpRequest.body
+    const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.addScheduledTimes.add(request)
     return await Promise.resolve(null) as any
   }
 }
