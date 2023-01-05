4b9a9cdc6da8e4cc912f3963b141c326d493f91c
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-05 11:30:04 -0300

    test: ensure DbAddScheduledTimes calls AddScheduledTimesRepository with correct values

--- src/data/tests/db/mock-add-scheduled-times-repository.ts
+++ src/data/tests/db/mock-add-scheduled-times-repository.ts
@@ -1,7 +1,12 @@
 import { AddScheduledTimes } from "../../../domain/usecases/add-scheduled-times";
+import { AddScheduledTimesRepository } from "../../protocols/db/add-scheduled-times-repository";
 
 export const mockAddScheduledTimesParams = (): AddScheduledTimes.Params => ({
   date: new Date(),
   time: 'any_time',
   accountId: 'any_account_id'
 })
+
+export class AddScheduledTimesRepositoryStub implements AddScheduledTimesRepository {
+  async add (params: AddScheduledTimes.Params): Promise<void> {}
+}
--- src/data/usecases/add-scheduled-times/db-add-scheduled-times.spec.ts
+++ src/data/usecases/add-scheduled-times/db-add-scheduled-times.spec.ts
@@ -1,19 +1,25 @@
 import { DbAddScheduledTimes } from './db-add-scheduled-times'
 import { LoadAccountRepositoryStub } from '../../tests/db/mock-load-account-repository'
 import { LoadAccountRepository } from '../../protocols/db/load-account-repository'
-import { mockAddScheduledTimesParams } from '../../tests/db/mock-add-scheduled-times-repository'
+import { AddScheduledTimesRepositoryStub, mockAddScheduledTimesParams } from '../../tests/db/mock-add-scheduled-times-repository'
+import { AddScheduledTimesRepository } from '../../protocols/db/add-scheduled-times-repository'
+
+jest.useFakeTimers().setSystemTime(new Date())
 
 type SutTypes = {
   sut: DbAddScheduledTimes
   loadAccountRepositoryStub: LoadAccountRepository
+  addScheduledTimesRepositoryStub: AddScheduledTimesRepository
 }
 
 const makeSut = (): SutTypes => {
   const loadAccountRepositoryStub = new LoadAccountRepositoryStub()
-  const sut = new DbAddScheduledTimes(loadAccountRepositoryStub)
+  const addScheduledTimesRepositoryStub = new AddScheduledTimesRepositoryStub()
+  const sut = new DbAddScheduledTimes(loadAccountRepositoryStub, addScheduledTimesRepositoryStub)
   return {
     sut,
-    loadAccountRepositoryStub
+    loadAccountRepositoryStub,
+    addScheduledTimesRepositoryStub
   }
 }
 
@@ -32,5 +38,16 @@ describe('DbAddScheduledTimes usecase', () => {
     })
     const promise = sut.add(mockAddScheduledTimesParams())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call AddScheduledTimesRepository with correct values', async () => {
+    const { sut, addScheduledTimesRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addScheduledTimesRepositoryStub, 'add')
+    await sut.add(mockAddScheduledTimesParams())
+    expect(addSpy).toHaveBeenCalledWith({
+      date: new Date(),
+      time: 'any_time',
+      accountId: 'any_account_id'
+    })
   })
 })
