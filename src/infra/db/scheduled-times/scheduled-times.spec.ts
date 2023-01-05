3ca98e2be24e34dfbdfe5ec29a9590898bdbd916
Lucas Vieira <lucas_99silva@hotmail.com>
2023-01-05 11:54:41 -0300

    test: ensure ScheduledTimesRepository add scheduled times on add success

--- src/infra/db/scheduled-times/scheduled-times.spec.ts
+++ src/infra/db/scheduled-times/scheduled-times.spec.ts
@@ -1,6 +1,8 @@
 import { client } from '../client'
 import { ScheduledTimesRepository } from './scheduled-times'
 
+jest.useFakeTimers().setSystemTime(new Date())
+
 describe('ScheduledTimesRepository', () => {
   beforeEach(async () => {
     await client.scheduledTimes.deleteMany()
@@ -12,22 +14,51 @@ describe('ScheduledTimesRepository', () => {
     await client.account.deleteMany()
   })
 
-  test('Should load all scheduled times on success', async () => {
-    const sut = new ScheduledTimesRepository()
-    const account = await client.account.create({
-      data: {
-        name: 'other_name',
-        email: 'other_email@mail.com',
-        password: 'other_password'
-      }
+  describe('loadAll()', () => {
+    test('Should load all scheduled times on success', async () => {
+      const sut = new ScheduledTimesRepository()
+      const account = await client.account.create({
+        data: {
+          name: 'other_name',
+          email: 'other_email@mail.com',
+          password: 'other_password'
+        }
+      })
+      await client.scheduledTimes.create({
+        data: {
+          time: '08:00',
+          account_id: account.id
+        }
+      })
+      const scheduledTimes = await sut.loadAll()
+      expect(scheduledTimes.length).toBe(1)
     })
-    await client.scheduledTimes.create({
-      data: {
-        time: '08:00',
-        account_id: account.id
-      }
+  })
+
+  describe('add()', () => {
+    test('Should add scheduled times on add success', async () => {
+      const sut = new ScheduledTimesRepository()
+      await client.account.create({
+        data: {
+          id: 'any_account_id',
+          name: 'other_name',
+          email: 'other_email@mail.com',
+          password: 'other_password'
+        }
+      })
+      await sut.add({
+        date: new Date(),
+        time: 'any_time',
+        accountId: 'any_account_id'
+      })
+      const scheduledTimes = await client.scheduledTimes.findFirst({
+        where: {
+          account_id: 'any_account_id'
+        }
+      })
+      expect(scheduledTimes?.date).toEqual(new Date())
+      expect(scheduledTimes?.time).toBe('any_time')
+      expect(scheduledTimes?.account_id).toBe('any_account_id')
     })
-    const scheduledTimes = await sut.loadAll()
-    expect(scheduledTimes.length).toBe(1)
   })
 })
